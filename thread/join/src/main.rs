a187af10ba3c74ec2b51576aa32765375d7d1a6f
Akagi201 <akagi201@gmail.com>
2018-08-02 14:25:30 +0800

    feat(thread): add join

---
+++ thread/join/src/main.rs
@@ -0,0 +1,26 @@
+use std::thread;
+use std::time::Duration;
+
+fn my_thread() {
+    println!("Thread {:?} is running", std::thread::current().id());
+    thread::sleep(Duration::from_millis(1));
+}
+
+fn main() {
+    let mut v = vec![];
+
+    for _i in 1..10 {
+        v.push(thread::spawn(|| {
+            my_thread();
+        }));
+    }
+
+    println!("main() waiting.");
+
+    for child in v {
+        match child.join() {
+            Ok(_) => (),
+            Err(why) => println!("Join failure {:?}", why),
+        };
+    }
+}
