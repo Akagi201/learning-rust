01a204ed70842f4d9465db8482a267b805d6bd4a
Akagi201 <akagi201@gmail.com>
2018-08-09 17:07:48 +0800

    feat: add some practices

---
+++ stdio/print_flush/src/main.rs
@@ -0,0 +1,11 @@
+use std::io::{self, Write};
+
+fn main() {
+    print!("请输入一个字符串: ");
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("读取失败");
+    print!("您输入的字符串是: {}\n", input);
+}
